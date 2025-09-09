339ac4493799c71cedc95fff5a89a8c31cb27bc5
MahmoudMadboly <mahmoudmadboly625@yahoo.com>
2025-09-09 13:27:21 +0300

    implement DBReader class

---
+++ TestUtomationFramework/src/main/java/config/DBReader.java
@@ -0,0 +1,109 @@
+package config;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DBReader extends FileBase{
+
+	private Connection connection;
+
+	private Statement statement;
+
+
+
+	public DBReader(String filePath, String userName, String password) {
+
+
+		// here filePath = JDBC connection URL
+		super(filePath);	
+
+
+
+	}
+
+
+	@Override
+	public void loadFile() {}
+
+
+
+
+
+	public void connectDB(String userName , String password) {
+
+		try {
+
+			//connect the data base
+			connection = DriverManager.getConnection(filePath, userName, password);
+
+			statement = connection.createStatement();
+
+
+		}catch(Exception e) {
+
+			e.printStackTrace();
+
+		}
+	}
+
+
+	public ResultSet executeQuery(String query) {
+
+
+		try {
+
+			return statement.executeQuery(query);
+
+		}catch(Exception e) {
+
+			e.printStackTrace();
+
+			return null;
+
+		}
+	}
+
+
+
+	public int executeUpdate(String query) {
+
+
+		try {
+
+			return statement.executeUpdate(query);
+
+		}catch(Exception e) {
+
+			e.printStackTrace();
+
+			return 0;
+
+		}
+	}
+
+
+	public void closeConnection() {
+
+
+		try {
+
+
+			if(statement != null)
+				statement.close();
+
+
+
+			if(connection != null)
+				connection.close();
+
+
+		}catch(Exception e) {
+
+			e.printStackTrace();
+
+		}
+	}
+}
---
+++ TestUtomationFramework/src/main/java/config/FileBase2.java
@@ -0,0 +1,25 @@
+package config;
+
+import java.io.FileInputStream;
+
+public class FileBase2 {
+	
+	protected String filePath;
+	
+public FileBase2(String filePath) {
+		
+		this.filePath = filePath;
+		
+	}
+
+public void fileStream() {
+	
+	FileInputStream file = new FileInputStream(filePath);
+	
+}
+	
+	
+	
+
+
+}
